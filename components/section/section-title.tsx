44d7320dcba1d840840e1d742fff73a3f979a681
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-01-20 15:44:22 +0100

    fix title

--- components/section/section-title.tsx
+++ components/section/section-title.tsx
@@ -7,11 +7,11 @@ interface Props {
 
 export const SectionTitle = ({ sectionNumber, label }: Props) => {
     return (
-        <h2 className="text-[24px] leading-[32px] sm:text-[32px] sm:leading-[40px]">
-            <span className={cn('font-bold text-primary', jura.className)}>
-                0.{sectionNumber}
-            </span>{' '}
-            - {label}
+        <h2 className="flex items-start px-4 text-[24px] leading-[32px] sm:text-[32px] sm:leading-[40px]">
+            <span className={cn('shrink-0 font-bold text-primary', jura.className)}>
+                0.{sectionNumber} -
+            </span>
+            <p className="ml-1">{label}</p>
         </h2>
     );
 };
