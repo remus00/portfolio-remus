aec8aebaf67d53d08aa92785d266ecca5bed9dc9
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-01-21 11:46:58 +0100

    fix

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -14,8 +14,10 @@ export async function POST(req: Request) {
             return Response.json(parsedBody.error.message, { status: 400 });
         }
 
+        console.log(body.email);
+
         const { data, error } = await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>',
+            from: `${body.fullName} <onboarding@resend.dev>`,
             to: ['info.remusburlacu@gmail.com'],
             subject: 'Mail from portfolio',
             react: EmailTemplate({
