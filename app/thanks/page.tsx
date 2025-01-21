78491f34e5d81bc3c6adca2b895133ad51bb4380
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-01-21 14:36:58 +0100

    add thank you page

---
+++ app/thanks/page.tsx
@@ -0,0 +1,24 @@
+import { Icon } from '@iconify/react/dist/iconify.js';
+import Link from 'next/link';
+
+export const ThanksPage = () => {
+    return (
+        <div className="flex h-screen w-full flex-col items-center justify-center gap-8 bg-background">
+            <div className="flex max-w-[395px] flex-col gap-4 text-center">
+                <h1 className="text-[40px] leading-[52px] tracking-[-0.25px] text-black dark:text-white sm:text-[52px] sm:leading-[65px]">
+                    <span className="text-primary">Thank you</span> for reaching out ♥️
+                </h1>
+                <p className="text-[20px] leading-5">Your message has been sent</p>
+            </div>
+            <Link
+                href="/"
+                className="inline-flex items-center text-muted-foreground transition-all duration-300 hover:text-primary"
+            >
+                <span className="text-[14px] leading-6 underline">Back to home page</span>
+                <Icon icon="ri:arrow-right-up-line" className="h-4 w-4" />
+            </Link>
+        </div>
+    );
+};
+
+export default ThanksPage;
--- services/mutation.ts
+++ services/mutation.ts
@@ -1,13 +1,18 @@
+'use client';
 import { Contacts } from '@/types/contacts';
 import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 import { sendEmail } from './api';
 
 export const useSendEmail = () => {
+    const router = useRouter();
+
     return useMutation({
         mutationFn: (data: Contacts) => sendEmail(data),
         onSuccess: () => {
             toast.success('Email sent successfully');
+            router.push('/thanks');
         },
         onError: () => {
             toast.error('Something went wrong');
