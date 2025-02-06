985ecdb1dfb30ac99cad1711aad3443808b77062
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-02-06 17:04:17 +0100

    update testimonials

--- constants/testimonials.ts
+++ constants/testimonials.ts
@@ -1,7 +1,24 @@
-import { MarcelloSilvetriAvatar, SalvatoreStamponeAvatar } from '@/public/testimonials';
+import {
+    FrancescoIntiniAvatar,
+    MarcelloSilvetriAvatar,
+    SalvatoreStamponeAvatar,
+} from '@/public/testimonials';
 import { Testimonial } from '@/types/testimonial';
 
 export const testimonialsData: Testimonial[] = [
+    {
+        fullName: 'Francesco Intini',
+        role: 'CTO | AI | NLP | Data Engineering',
+        avatar: FrancescoIntiniAvatar,
+        linkedInHref: 'https://www.linkedin.com/in/francesco-intini-39576b175/',
+        text: `I had the pleasure of working closely with Remus during his tenure Wolico, where I served as the CTO. I am writing to highly recommend Remus, who served primarily as our Frontend Developer. While he may not be the most technically seasoned developer in the field, Remus compensates for his relative lack of seniority and technical expertise with boundless enthusiasm and a steadfast commitment to continuous learning. His proactive approach to acquiring new skills and deepening his technical knowledge has been a significant asset to our team.
+
+Remus consistently demonstrates the passion of a designer combined with the rigor of a developer. His meticulous attention to detail and respect for design principles have ensured that his work is consistently pixel-perfect. This unique blend of creativity and technical diligence has made him an invaluable contributor to our projects, particularly in maintaining and advancing our design systems.
+
+Moreover, Remus is not only an excellent individual contributor but also a strong team player. His ability to work independently while effectively collaborating within a team environment makes him a versatile and reliable resource for any technical team.
+
+I am confident that Remus will continue to excel and bring significant value to any organization fortunate enough to have him on board.`,
+    },
     {
         fullName: 'Marcello Silvestri',
         role: 'Entrepreneur, full stack web developer and marketing strategist',
@@ -15,11 +32,8 @@ export const testimonialsData: Testimonial[] = [
         avatar: SalvatoreStamponeAvatar,
         linkedInHref: 'https://www.linkedin.com/in/salvatorestampone/',
         text: `Working with Remus for several months on a high-intensity project for the Italian Football League (Serie A, Serie B, and AIA Section) was a pleasure. When I arrived, Remus had already developed a great foundation for the front-end. We worked closely together further improving the application, at the aesthetic level as much as at the functional level.
-
 His mastery of Next.js, TailwindCSS and the latest paradigms and best practices emerged from day one. Not only that: Remus seems naturally talented for UI and UX design.
-
 On a personal level, you will appreciate his unwavering calm. Tight deadlines, changing priorities, and unexpected challenges: Remus faced it all with patience, focus, and determination. And with a healthy dose of humor, from which the whole team benefited!
-
 I genuinely recommend working Remus: he is a talented professional, standing for reliability and consistency.`,
     },
 ];
--- public/testimonials/index.ts
+++ public/testimonials/index.ts
@@ -1,5 +1,11 @@
 import AvatarPlaceholder from './avatar-placeholder.png';
+import FrancescoIntiniAvatar from './francesco-intini.jpg';
 import MarcelloSilvetriAvatar from './marcello-silvestri.jpeg';
 import SalvatoreStamponeAvatar from './salvatore-stampone.jpeg';
 
-export { AvatarPlaceholder, MarcelloSilvetriAvatar, SalvatoreStamponeAvatar };
+export {
+    AvatarPlaceholder,
+    FrancescoIntiniAvatar,
+    MarcelloSilvetriAvatar,
+    SalvatoreStamponeAvatar,
+};
